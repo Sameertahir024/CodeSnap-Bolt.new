0bd256f5668db500736f8a0a833571c022829f29
sameer tahir <sameertahir024@gmail.com>
2025-01-19 20:41:00 +0500

    add authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
+import Footer from "@/components/landing/Footer";
+import Hero from "@/components/landing/Hero";
 import { MarqueeDemoVertical } from "@/components/landing/MarqueeDemoVertical";
 import Navbar from "@/components/landing/Navbar";
-import { Button } from "@/components/ui/button";
+import Pricing from "@/components/landing/Pricing";
 
 export default function Home() {
   return (
     <div>
-      <Button>Click me</Button>
       <Navbar />
-      <MarqueeDemoVertical />
+      <Hero />
+      <Pricing />
+      {/* <MarqueeDemoVertical /> */}
+      <Footer />
     </div>
   );
 }
--- src/components/landing/Navbar.tsx
+++ src/components/landing/Navbar.tsx
@@ -2,12 +2,15 @@
 import React from "react";
 import { useTheme } from "next-themes";
 import { Button } from "../ui/button";
+import { toast } from 'sonner';
+
 
 const Navbar = () => {
   const { setTheme } = useTheme();
 
   return (
     <div>
+      <button onClick={() => toast('Toast')}>Render Toast</button>;
       <Button onClick={() => setTheme("dark")}>dark</Button>
       <Button onClick={() => setTheme("light")}>light</Button>
     </div>

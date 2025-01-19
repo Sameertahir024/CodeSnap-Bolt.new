1f43073e7aa6840569b286dcb581fd4823e16bd5
sameer tahir <sameertahir024@gmail.com>
2025-01-19 21:06:03 +0500

    fix marqee

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <Pricing />
-      {/* <MarqueeDemoVertical /> */}
+      <MarqueeDemoVertical />
       <Footer />
     </div>
   );

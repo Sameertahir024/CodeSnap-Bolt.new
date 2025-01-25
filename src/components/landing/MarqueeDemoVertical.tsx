25128dbe1af6f280b0cbac4ed1340e335d2c28c4
sameer tahir <sameertahir024@gmail.com>
2025-01-25 13:18:51 +0500

    Add testimonial section with vertical marquee and user reviews

--- src/components/landing/MarqueeDemoVertical.tsx
+++ src/components/landing/MarqueeDemoVertical.tsx
@@ -111,29 +111,39 @@ const ReviewCard = ({
 
 export function MarqueeDemoVertical() {
   return (
-    <div className="relative flex flex-wrap h-[500px] w-full flex-row items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
-      <Marquee pauseOnHover vertical className="[--duration:20s]">
-        {firstRow.map((review) => (
-          <ReviewCard key={review.username} {...review} />
-        ))}
-      </Marquee>
-      <Marquee reverse pauseOnHover vertical className="[--duration:20s]">
-        {secondRow.map((review) => (
-          <ReviewCard key={review.username} {...review} />
-        ))}
-      </Marquee>
-      <Marquee pauseOnHover vertical className="[--duration:20s]">
-        {firstRow.map((review) => (
-          <ReviewCard key={review.username} {...review} />
-        ))}
-      </Marquee>
-      <Marquee reverse pauseOnHover vertical className="[--duration:20s]">
-        {secondRow.map((review) => (
-          <ReviewCard key={review.username} {...review} />
-        ))}
-      </Marquee>
-      <div className="pointer-events-none absolute inset-x-0 top-0 h-1/3 bg-gradient-to-b from-white dark:from-background"></div>
-      <div className="pointer-events-none absolute inset-x-0 bottom-0 h-1/3 bg-gradient-to-t from-white dark:from-background"></div>
+    <div className="my-16">
+      <div>
+      <h1 className="text-3xl font-medium tracking-tight text-neutrl-900 dark:text-white sm:text-center">
+        Loved by thousands of people
+      </h1>
+      <p className="mt-2 text-lg text-neutral-600 dark:text-neutral-200 sm:text-center">
+        Here's what some of our users have to say about CodeSnap.
+      </p>
+      </div>
+      <div className="relative my-32 flex flex-wrap h-[500px] w-full flex-row items-center justify-center overflow-hidden rounded-lg   md:shadow-xl">
+        <Marquee pauseOnHover vertical className="[--duration:20s]">
+          {firstRow.map((review) => (
+            <ReviewCard key={review.username} {...review} />
+          ))}
+        </Marquee>
+        <Marquee reverse pauseOnHover vertical className="[--duration:20s]">
+          {secondRow.map((review) => (
+            <ReviewCard key={review.username} {...review} />
+          ))}
+        </Marquee>
+        <Marquee pauseOnHover vertical className="[--duration:20s]">
+          {firstRow.map((review) => (
+            <ReviewCard key={review.username} {...review} />
+          ))}
+        </Marquee>
+        <Marquee reverse pauseOnHover vertical className="[--duration:20s]">
+          {secondRow.map((review) => (
+            <ReviewCard key={review.username} {...review} />
+          ))}
+        </Marquee>
+        <div className="pointer-events-none absolute inset-x-0 top-0 h-1/3 bg-gradient-to-b from-white dark:from-background"></div>
+        <div className="pointer-events-none absolute inset-x-0 bottom-0 h-1/3 bg-gradient-to-t from-white dark:from-background"></div>
+      </div>
     </div>
   );
 }

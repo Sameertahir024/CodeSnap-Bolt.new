0d5ade7db5be51ddf9a51c06cbbee5a5f221cae2
sameer tahir <sameertahir024@gmail.com>
2025-01-26 15:02:39 +0500

    new add

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone", // Instead of 'export'
+
   images: {
     domains: ["avatar.vercel.sh"], // Allow external images from this domain
   },
